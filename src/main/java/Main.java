4721b377f36538ed2c0b82865ae0799625c290cc
지원오 <wonoh@zuminternet.com>
2020-02-13 18:04:34 +0900

    feat: main 클래스 생성

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import lotto.generator.LottoGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        lottoGenerator.makeLotto();
+
+    }
+}
