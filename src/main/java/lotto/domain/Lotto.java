1b04d6d4c2cfd02f8b4445fbc382b6e2f44c6d27
wonoh <wldnjsdh44121@nate.com>
2020-02-15 11:35:56 +0900

    feat: 문자열 처리 로직,매치갯수 계산 로직 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
@@ -13,12 +14,25 @@ public class Lotto {
     public static Lotto of(List<Integer> numbers){
         return new Lotto(numbers);
     }
-
-    @Override
-    public String toString() {
-        return "Lotto{" +
-                "numbers=" + numbers +
-                '}';
+    public static Lotto of(String inputNumbers){
+        String[] split = inputNumbers.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (String s : split) {
+            numbers.add(Integer.parseInt(s.trim()));
+        }
+        return new Lotto(numbers);
+    }
+    public int matchCount(Lotto winningLotto){
+        int matchCount = 0;
+        for (Integer number : this.numbers) {
+            if(winningLotto.contains(number)){
+                matchCount++;
+            }
+        }
+        return matchCount;
+    }
+    private boolean contains(Integer number){
+        return this.numbers.contains(number);
     }
 
     public List<Integer> getNumbers() {
