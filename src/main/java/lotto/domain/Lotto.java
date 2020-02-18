ccf5a2656d9a1bd573de8c0c7cab68b7da13618b
지원오 <wonoh@zuminternet.com>
2020-02-19 08:06:12 +0900

    refactor: comma 상수 분리 및 stream 사용

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,35 +1,31 @@
 package lotto.domain;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
 
     private List<Integer> numbers;
+    private static final String COMMA = ",";
 
     private Lotto(List<Integer> numbers){
         this.numbers = numbers;
     }
-
     public static Lotto of(List<Integer> numbers){
         return new Lotto(numbers);
     }
     public static Lotto of(String inputNumbers){
-        String[] split = inputNumbers.split(",");
-        List<Integer> numbers = new ArrayList<>();
-        for (String s : split) {
-            numbers.add(Integer.parseInt(s.trim()));
-        }
+        List<Integer> numbers = Arrays.stream(inputNumbers.split(COMMA))
+                .map(s -> Integer.parseInt(s.trim()))
+                .collect(Collectors.toList());
         return new Lotto(numbers);
     }
     public int matchCount(Lotto winningLotto){
-        int matchCount = 0;
-        for (Integer number : this.numbers) {
-            if(winningLotto.contains(number)){
-                matchCount++;
-            }
-        }
-        return matchCount;
+        return (int) this.numbers
+                .stream()
+                .filter(winningLotto::contains)
+                .count();
     }
     private boolean contains(Integer number){
         return this.numbers.contains(number);
