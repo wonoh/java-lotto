2b7c7f028dac2133f675340010b8ecc9901d7746
지원오 <wonoh@zuminternet.com>
2020-02-24 13:54:37 +0900

    feat: 보너스번호 추가 및 유효성 검증

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -3,15 +3,29 @@ package lotto.domain;
 public class WinningLotto {
 
     private Lotto lotto;
+    private Integer bonusNumber;
 
-    private WinningLotto(Lotto lotto){
+    private WinningLotto(Lotto lotto,Integer bonusNumber){
         this.lotto = lotto;
+        validateBonusNumber(bonusNumber);
+        this.bonusNumber = bonusNumber;
     }
-    public static WinningLotto of(String inputNumbers){
-        return new WinningLotto(Lotto.of(inputNumbers));
+    public static WinningLotto of(String inputNumbers,String bonusNumber){
+        return new WinningLotto(
+                Lotto.of(inputNumbers),
+                Integer.valueOf(bonusNumber)
+        );
+    }
+    public Integer getBonusNumber() {
+        return bonusNumber;
     }
 
     public Lotto lotto() {
         return lotto;
+    }
+    private void validateBonusNumber(Integer bonusNumber){
+        if(this.lotto.contains(bonusNumber)){
+            throw new IllegalArgumentException("중복된 번호입니다.");
+        }
     }
 }
