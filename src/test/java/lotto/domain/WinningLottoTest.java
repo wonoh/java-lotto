3d1cc18b85e9c2c5efde5c93b371e1da382487b9
지원오 <wonoh@zuminternet.com>
2020-02-24 13:53:40 +0900

    test: WinningLotto 생성 테스트

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -2,18 +2,29 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DisplayName("지난주 당첨 로또 테스트")
 class WinningLottoTest {
 
-    @DisplayName("당첨 로또를 입력받아 객체를 생성한다.")
+    private static Stream<Arguments> winningLottoProvider(){
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6","8"),
+                Arguments.of("1,2,3,4,5,6","9"),
+                Arguments.of("1,2,3,4,5,6","10")
+        );
+    }
+
+    @DisplayName("당첨 로또,보너스 번호를 입력받아 객체를 생성한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,2,3,4,5,6","3,5,6,7,8,9"})
-    void of(String inputNumbers) {
-        WinningLotto winningLotto = WinningLotto.of(inputNumbers);
+    @MethodSource(value = "winningLottoProvider")
+    void of(String inputNumbers,String bonusNumber) {
+        WinningLotto winningLotto = WinningLotto.of(inputNumbers,bonusNumber);
         assertNotNull(winningLotto);
     }
 
