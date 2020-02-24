7242b32d4443ff3620ab1d4f438a77c57290bc13
지원오 <wonoh@zuminternet.com>
2020-02-24 13:52:35 +0900

    test: WinningLotto 생성 테스트

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -2,10 +2,13 @@ package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -20,13 +23,20 @@ class LottoStoreTest {
         LottoStore lottoStore = createLottoStore(inputNumbers);
         assertNotNull(lottoStore);
     }
+    private static Stream<Arguments> winningLottoProvider(){
+        return Stream.of(
+                Arguments.of("1,2,3,4,5,6","8"),
+                Arguments.of("1,2,3,4,5,6","9"),
+                Arguments.of("1,2,3,4,5,6","10")
+        );
+    }
 
     @DisplayName("지난주 당첨로또로 결과를 생성한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,2,3,4,5,6","3,5,6,7,8,9"})
-    void createResults(String inputNumbers) {
+    @MethodSource(value = "winningLottoProvider")
+    void createResults(String inputNumbers,String bonusNumber) {
         LottoStore lottoStore = createLottoStore(inputNumbers);
-        WinningLotto winningLotto = WinningLotto.of(inputNumbers);
+        WinningLotto winningLotto = WinningLotto.of(inputNumbers,bonusNumber);
         Results results = lottoStore.createResults(winningLotto);
         assertNotNull(results);
     }
