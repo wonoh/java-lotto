085cd5bcc5428645ebbb5f8db480fcf0a353244e
wonoh <wonoh@zuminternet.com>
2020-02-26 21:32:12 +0900

    refactor: 로또 범위 유효성검증 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,19 +1,21 @@
 package lotto.domain;
 
+import static lotto.domain.constant.LottoConstants.*;
+
 public class WinningLotto {
 
     private Lotto lotto;
-    private Integer bonusNumber;
+    private int bonusNumber;
 
-    private WinningLotto(Lotto lotto,Integer bonusNumber){
+    private WinningLotto(Lotto lotto,int bonusNumber){
         this.lotto = lotto;
         validateBonusNumber(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
-    public static WinningLotto of(String inputNumbers,String bonusNumber){
+    public static WinningLotto of(String inputNumbers,int bonusNumber){
         return new WinningLotto(
                 Lotto.of(inputNumbers),
-                Integer.valueOf(bonusNumber)
+                bonusNumber
         );
     }
     public Integer getBonusNumber() {
@@ -25,7 +27,11 @@ public class WinningLotto {
     }
     private void validateBonusNumber(Integer bonusNumber){
         if(this.lotto.contains(bonusNumber)){
-            throw new IllegalArgumentException("중복된 번호입니다.");
+            throw new IllegalArgumentException(bonusNumber+"는 중복된 번호입니다.");
         }
+        if(bonusNumber <= LOTTO_START_NUMBER || bonusNumber >= LOTTO_END_NUMBER){
+            throw new IllegalArgumentException(bonusNumber+"는 로또 숫자 범위에서 벗어납니다.");
+        }
+
     }
 }
---
+++ src/main/java/lotto/domain/constant/LottoConstants.java
@@ -0,0 +1,11 @@
+package lotto.domain.constant;
+
+public class LottoConstants {
+
+    private LottoConstants() {
+    }
+
+    public static final int LOTTO_START_NUMBER = 1;
+    public static final int LOTTO_END_NUMBER = 45;
+
+}
