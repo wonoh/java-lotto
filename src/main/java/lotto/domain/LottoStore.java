78a0a8e6edfd7e20be8b5349816aeaeb78277046
wonoh <wonoh@zuminternet.com>
2020-02-27 17:38:43 +0900

    refactor: 파라미터 검증

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -2,13 +2,18 @@ package lotto.domain;
 
 import lotto.domain.type.ResultType;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+
+import static lotto.domain.constant.LottoConstants.LOTTO_SIZE;
 
 public class LottoStore {
 
     private final List<Lotto> lottos;
 
     private LottoStore(List<Lotto> lottos){
+        validateLottos(lottos);
         this.lottos = lottos;
     }
     public static LottoStore of(List<Lotto> lottos){
@@ -22,8 +27,12 @@ public class LottoStore {
         }
         return results;
     }
-
+    private void validateLottos(List<Lotto> lottos){
+        if(Objects.isNull(lottos)){
+            throw new IllegalArgumentException("로또번호는 존재해야 합니다.");
+        }
+    }
     public List<Lotto> getLottos() {
-        return lottos;
+        return Collections.unmodifiableList(lottos);
     }
 }
