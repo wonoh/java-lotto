ba855ad5fa13a3fb2ddaeaf6d7650f56a2589725
wonoh <wonoh@zuminternet.com>
2020-02-27 17:39:38 +0900

    refactor: 로또 사이즈 상수정의

--- src/main/java/lotto/domain/constant/LottoConstants.java
+++ src/main/java/lotto/domain/constant/LottoConstants.java
@@ -7,5 +7,6 @@ public class LottoConstants {
 
     public static final int LOTTO_START_NUMBER = 1;
     public static final int LOTTO_END_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
 
 }
