da8a5a0b083aeb21a444930eb4f65112712340f4
wonoh <wonoh@zuminternet.com>
2020-03-07 20:31:26 +0900

    refactor: scanner.nextLine() 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -24,17 +24,20 @@ public class InputView {
     }
     public static int inputManualLottoCount(){
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
-        return scanner.nextInt();
+        int inputManualLottoCount = scanner.nextInt();
+        scanner.nextLine();
+        return inputManualLottoCount;
     }
     public static List<String> inputManualLottoNumbers(int manualLottoCount){
         System.out.println("수동으로 구매할 번호를 입력해 주세요.");
-        scanner.nextLine();
         return IntStream.range(0,manualLottoCount)
                 .mapToObj(i->scanner.nextLine())
                 .collect(Collectors.toList());
     }
     public static int inputBonusNumber(){
         System.out.println("보너스 볼을 입력해 주세요.");
-        return scanner.nextInt();
+        int inputBonusNumber = scanner.nextInt();
+        scanner.nextLine();
+        return inputBonusNumber;
     }
 }
